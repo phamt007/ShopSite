ce0f1e832f1f8724d581ec417ee2a428a23b1f25
Tim Pham <43245834+phamt007@users.noreply.github.com>
2025-03-04 12:12:25 -0800

    Update page.tsx

--- app/admin/products/[id]/edit/page.tsx
+++ app/admin/products/[id]/edit/page.tsx
@@ -3,10 +3,11 @@ import { PageHeader } from "../../../_components/PageHeader"
 import { ProductForm } from "../../_components/ProductForm"
 
 export default async function EditProductPage({
-  params: { id },
+  params,
 }: {
-  params: { id: string }
+  params: Promise<{ id: string }>
 }) {
+  const { id } = await params
   const product = await db.product.findUnique({ where: { id } })
 
   return (
